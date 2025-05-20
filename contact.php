3dc7434c7d885f27e3ad08e58835d0bd914a6bec
Wladman280 <wladman7@gmail.com>
2025-05-20 11:19:35 +0300

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,38 +1,46 @@
-<? 
+<?php 
 
-if (isset($_POST['name'])) {$name = $_POST['name'];}
-if (isset($_POST['phone'])) {$phone = $_POST['phone'];}
-if (isset($_POST['email'])) {$email = $_POST['email'];}
-if (isset($_POST['message'])) {$message = $_POST['message'];}
+if (isset($_POST['name'])) {
+    $name = stripslashes(htmlspecialchars($_POST['name']));
+} else {
+    $name = '';
+}
 
+if (isset($_POST['phone'])) {
+    $phone = stripslashes(htmlspecialchars($_POST['phone']));
+} else {
+    $phone = '';
+}
 
-$name = stripslashes($name);
+if (isset($_POST['email'])) {
+    $email = stripslashes(htmlspecialchars($_POST['email']));
+} else {
+    $email = '';
+}
 
-$name = htmlspecialchars($name);
+if (isset($_POST['message'])) {
+    $message = stripslashes(htmlspecialchars($_POST['message']));
+} else {
+    $message = '';
+}
 
-$phone = stripslashes($phone);
+// Ваш адрес
+$address = "wlad2806@mail.com"; 
 
-$phone = htmlspecialchars($phone);
+// Тема письма
+$subject = "Новое сообщение с сайта"; 
 
+// Формат сообщения
+$body = "Имя: ".$name."\nМой контактный e-mail: ".$email."\nТелефон: ".$phone."\nСообщение: ".$message;
 
-$email = stripslashes($email);
+// Отправка почты
+$verify = mail($address, $subject, $body, "Content-type:text/plain; Charset=utf-8\r\n");
 
-$message = stripslashes($message);
-
-
-$email = htmlspecialchars($email);
-
-$message = htmlspecialchars($message);
-
-
-
-
-
-
-$address = "wlad2806@mail.com"; // ВАШ АДРЕС
-
-$message = "Имя: ".$name."\nМой контактный e-mail: ".$email."\nТелефон: ".$phone."\nСообщение: ".$message."";
-
-$verify = mail($address,$subject,$message,"Content-type:text/plain; Charset=utf-8\r\n");
+// Проверка успешности отправки
+if ($verify) {
+    echo "Сообщение успешно отправлено!";
+} else {
+    echo "Ошибка при отправке сообщения.";
+}
 
 ?>
